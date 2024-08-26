d625a3fb7644c33d7fa54829ae853568fe96c3ca
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2024-08-26 15:14:19 +0000

    add loadtest example

--- project/setup_dashboard.sh
+++ project/setup_dashboard.sh
@@ -5,7 +5,11 @@ ray stop
 ray metrics launch-prometheus
 
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-11.1.4.linux-amd64.tar.gz
-tar -zxvf grafana-enterprise-11.1.4.linux-amd64.tar.gz
+tar -zxvf grafana-*.tar.gz
 ./grafana-v11.1.4/bin/grafana-server --homepath grafana-v11.1.4 --config /tmp/ray/session_latest/metrics/grafana/grafana.ini
 
-RAY_GRAFANA_HOST=https://bookish-meme-grx6xp4jvjrc9qx4-3000.app.github.dev/?orgId=1 RAY_PROMETHEUS_HOST=https://bookish-meme-grx6xp4jvjrc9qx4-9090.app.github.dev ray start --head
+RAY_GRAFANA_HOST=https://redesigned-chainsaw-q655pxwjqq3r7v-3000.app.github.dev RAY_PROMETHEUS_HOST=https://redesigned-chainsaw-q655pxwjqq3r7v-9090.app.github.dev/ ray start --head
+
+serve run src.server:entrypoint
+
+locust -f load_test/average_test.py --host=http://127.0.0.1:8000
